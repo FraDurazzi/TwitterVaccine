19dcf09f23a6cac880ff50aea484f19bf013abe2
Mauro Faccin <mauro.fccn@gmail.com>
2024-05-15 13:04:02 +0200

    [feat] add code to compute the Laplacian eigenvectors
    The python conde computes the adjacency matrix
    The octave code computes the eigenvectors (muuuch faster)

---
+++ build_embeddings_eigs.m
@@ -0,0 +1,46 @@
+%% this script will compute the smaller eigenvectors of the laplacian.
+% There was a need of rewriting this in Octave because scipy is very slow
+
+DEADLINES = {
+    "test",
+    "2021-06-01",
+    "2022-01-01",
+    "2022-07-01",
+    "2023-01-01",
+    "2024-01-01",
+};
+
+NEIGS = 10;
+
+for i = 1:rows(DEADLINES)
+    fprintf("==== %d deadline %s\n", i, DEADLINES{i})
+    fname = sprintf("./data/adj_%s.mat.gz", DEADLINES{i});
+
+    % read the adjacency matrix as a list of weighted edges (i, j, A_ij)
+    M = load(fname);
+
+    % increase indexing (Octave count from 1)
+    M(:, 1) = M(:, 1) + 1;
+    M(:, 2) = M(:, 2) + 1;
+
+    % define a sparse matrix
+    mat = spconvert(M);
+
+    % compute the degree
+    deg = sum(mat);
+    dim = columns(mat);
+
+    % laplacian
+    lap = diag(deg) - mat;
+
+    % compute the eigenpairs
+    % opt.tol=1e-9;
+    opt.issym=true;
+    [bevecs, bevals] = eigs(lap, NEIGS + 1, 'sm', opt);
+    bevals = diag(bevals);
+
+    % drop the Frobenious eigenvector
+    bevecs = bevecs(:, 1:NEIGS);
+    fname = sprintf("./data/embedding_laplacian_%s.txt.gz", DEADLINES{i});
+    save("-ascii","-zip", fname, "bevecs");
+end
---
+++ build_embeddings_eigs.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+"""Eigenvector embeddings."""
+
+import time
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scikits import umfpack
+from scipy import sparse
+from scipy.sparse import linalg
+
+from build_graphs import DEADLINES, load_graph
+
+NUM_EIGS = 11
+
+
+def find_smaller_eigenvectors(
+    matrix: sparse.spmatrix, neig: int
+) -> tuple[np.ndarray, np.ndarray]:
+    """Find the `neig` eigpairs corresponding to the smallerst eigenvalues."""
+    # fing largest eigenvalue (pos def matrix has all positive evals)
+    eval = linalg.eigsh(matrix, k=1, which="LM", tol=1e-5)[0][0]
+
+    # flip-shift the eigenvalues
+    linalg.use_solver(useUmfpack=True)
+
+    # find eigval eigvec
+    evals, evecs = linalg.eigsh(
+        eval * sparse.eye(matrix.shape[0]) - matrix,
+        k=neig,
+        which="LM",
+        sigma=eval,
+        tol=1e-5,
+        v0=np.ones(matrix.shape[0]),
+    )
+    return eval - evals, evecs
+
+
+def main() -> None:
+    """Do the main."""
+    fig, axs = plt.subplots(ncols=len(DEADLINES), sharex=True, sharey=True)
+    for deadline, ax in zip(DEADLINES, axs):
+        print(deadline)
+        tail, head, usermap = load_graph(deadline)
+        comms = pd.read_csv(
+            f"data/communities_{deadline}.csv.gz", index_col="user_index"
+        )
+
+        adj = (tail @ head.T).astype(np.float64)
+        adj = adj + adj.T  # symmetrize
+
+        _adj = adj.tocoo()
+        data = pd.DataFrame({"i": _adj.row, "j": _adj.col, "val": _adj.data}, dtype=int)
+        data.to_csv(f"data/adj_{deadline}.m.gz", index=False, header=False, sep=" ")
+        continue
+
+        print(adj.nnz, adj.shape)
+        print("Density", adj.nnz / adj.shape[0] ** 2)
+
+        degree = sparse.diags(adj.sum(axis=1).A1, shape=adj.shape, offsets=0)
+
+        t0 = time.time()
+        eigenvalues, eigenvectors = find_smaller_eigenvectors(
+            degree - adj, neig=NUM_EIGS
+        )
+        print(time.time() - t0)
+        print(eigenvalues)
+
+        t0 = time.time()
+        eigenvalues, eigenvectors = linalg.eigsh(
+            degree - adj,
+            k=NUM_EIGS,
+            which="LM",
+            sigma=0,
+            tol=1e-5,
+            v0=np.ones(adj.shape[0]),
+        )
+        print(time.time() - t0)
+        print(eigenvalues)
+        print(eigenvectors)
+
+        # eigenvalues = -eigenvalues
+        eigenvectors /= np.abs(eigenvectors).sum(0)
+
+        eigs = pd.concat(
+            [pd.DataFrame({"eigvalues": eigenvalues}).T, pd.DataFrame(eigenvectors)]
+        )
+        eigs.to_csv(f"./data/embedding_laplacian_{deadline}.csv.gz")
+
+        ax.scatter(
+            eigenvectors[:, 1],
+            eigenvectors[:, 2],
+            c=comms["louvain"],
+            s=1,
+            alpha=0.1,
+            cmap="rainbow",
+        )
+        ax.set(title=deadline)
+        # ax.loglog()
+    plt.savefig("data/embedding_laplacian.pdf")
+
+
+if __name__ == "__main__":
+    main()
