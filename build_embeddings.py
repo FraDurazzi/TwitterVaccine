8c1abcdcf56310fe6963b5a97befeeb6b48229d3
Mauro Faccin <mauro.fccn@gmail.com>
2024-05-15 13:05:09 +0200

    [feat] compute the force atlas embetting

---
+++ build_embeddings.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""Starting from the graphs, compute the community structures."""
+
+from __future__ import annotations
+
+import numpy as np
+import pandas as pd
+from fa2_modified import ForceAtlas2
+from scipy import sparse
+
+from build_graphs import DEADLINES, load_graph
+
+
+def main() -> None:
+    """Do the main."""
+    positions: dict[str, pd.DataFrame] = {}
+    for deadline in DEADLINES:
+        print(deadline)
+        tail, head, usermap = load_graph(deadline)
+        print("N users =", tail.shape[0])
+        print("N edges =", head.nnz, tail.nnz)
+
+        adj = tail @ head.T
+
+        if deadline == DEADLINES[0]:
+            # use a random initial position
+            pos = embed_fa2(adj, init_pos=None, n_cycles=1000)
+        elif deadline == DEADLINES[1]:
+            # use positions from `test`
+            init_pos = set_initial_pos(positions[DEADLINES[0]], usermap)
+            pos = embed_fa2(
+                adj,
+                init_pos=init_pos,
+                n_cycles=300,
+            )
+        else:
+            # use positions from `2021-06-01`
+            init_pos = set_initial_pos(positions[DEADLINES[1]], usermap)
+            pos = embed_fa2(
+                adj,
+                init_pos=init_pos,
+                n_cycles=100,
+            )
+
+        positions[deadline] = pd.concat([usermap, pos], axis=1)
+
+        if deadline != DEADLINES[0]:
+            pass
+
+        positions[deadline].to_csv(f"data/embedding_fa2_{deadline}.csv.gz")
+
+
+def set_initial_pos(old_pos: pd.DataFrame, new_usermap: pd.Series) -> pd.DataFrame:
+    """Find old positions for the new map."""
+    rng = np.random.default_rng()
+
+    # initially set random positions
+    new_pos = pd.concat(
+        [
+            new_usermap,
+            pd.DataFrame(
+                {
+                    "fa2_x": rng.choice(old_pos["fa2_x"], size=len(new_usermap)),
+                    "fa2_y": rng.choice(old_pos["fa2_y"], size=len(new_usermap)),
+                }
+            ),
+        ],
+        axis=1,
+    )
+
+    # the user_index should be incremental hence just use the user_id
+    new_pos = new_pos.set_index("user_id")
+
+    # same
+    op = old_pos
+    op = op.set_index("user_id")
+
+    # assign the old position to user present in the previous step.
+    new_pos.loc[op.index] = op
+
+    # set the user_index as a incremental value
+    new_pos = new_pos.reset_index(drop=True)
+    new_pos.index.name = "user_index"
+    return new_pos
+
+
+def embed_fa2(
+    adj: sparse.spmatrix, init_pos: pd.DataFrame | None, n_cycles: int = 50
+) -> pd.DataFrame:
+    """Embed into a 2D."""
+    fa = ForceAtlas2()
+    if init_pos is None:
+        pos = None
+    else:
+        pos = np.array([(x["fa2_x"], x["fa2_y"]) for _, x in init_pos.iterrows()])
+    pos = fa.forceatlas2(adj + adj.T, pos=pos, iterations=n_cycles)
+    return pd.DataFrame(pos, columns=["fa2_x", "fa2_y"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ build_embeddings_refine.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+
+import asyncio
+import pathlib
+from concurrent.futures import ThreadPoolExecutor as Executor
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+from build_embeddings import embed_fa2
+from build_graphs import DEADLINES, load_graph
+
+MAX_LIM = 10000
+
+
+def load(deadline: str) -> pd.DataFrame:
+    """Load updated positions if available otherwise the old ones."""
+    old_pos = pd.read_csv(f"data/embedding_fa2_{deadline}.csv.gz", index_col=0)
+
+    path = pathlib.Path(f"data/embedding_fa2_{deadline}_refined.csv.gz")
+    if not path.exists():
+        print("Use old positions")
+        return old_pos
+
+    print("Start from updated positions")
+    new_pos = pd.read_csv(path, index_col=0)
+    new_pos["user_id"] = old_pos["user_id"]
+    return new_pos
+
+
+def refine_positions(
+    deadline: str, deadline_fix: str | None = None, cycles: int = 10
+) -> None:
+    """Refine and save positions with optional fixed positions."""
+    positions = load(deadline)
+
+    # communities (for plotting)
+    comms = pd.read_csv(f"data/communities_{deadline}.csv.gz", index_col="user_index")
+
+    # linking pattern (to calculate the adjacency matrix)
+    tail, head, usermap = load_graph(deadline)
+    adj = tail @ head.T
+
+    # load fixed positions
+    if deadline_fix is not None:
+        fixed_pos = load(deadline_fix)
+        fixed_pos = (
+            fixed_pos.set_index("user_id")
+            .reindex(positions["user_id"])  # align to positions
+            .reset_index()
+            .set_index(positions.index)
+            .dropna()
+        )
+
+    for i in range(cycles):
+        print(f" Cycle {i}  for {deadline}")
+        new_pos = embed_fa2(adj, positions, n_cycles=50)
+        new_pos.to_csv(f"./data/embedding_fa2_{deadline}_refined.csv.gz")
+
+        positions = new_pos
+
+        if deadline_fix is not None:
+            positions.loc[new_pos.index] = new_pos
+
+        asyncio.run(
+            plot_positions(
+                pd.concat([positions, comms], axis=1),
+                f"./data/embedding_fa2_{deadline}_refined.png",
+            )
+        )
+
+
+async def plot_positions(positions: pd.DataFrame, fname: str) -> None:
+    """Plot."""
+    fig, ax = plt.subplots(
+        figsize=(10, 10),
+        gridspec_kw={"left": 0, "right": 1, "bottom": 0, "top": 1},
+    )
+    ax.set_axis_off()
+    ax.scatter(
+        positions["fa2_x"],
+        positions["fa2_y"],
+        c=positions["leiden"],
+        s=1,
+        alpha=0.1,
+        cmap="rainbow",
+    )
+    ax.set(xlim=(-MAX_LIM, MAX_LIM), ylim=(-MAX_LIM, MAX_LIM))
+    plt.savefig(fname, dpi=300)
+
+
+def main() -> None:
+    """Do the main."""
+
+    refine_positions(DEADLINES[1], cycles=20)
+
+    with Executor(max_workers=8) as exc:
+        for deadline in DEADLINES[2:]:
+            exc.submit(refine_positions, deadline, deadline_fix=DEADLINES[1], cycles=10)
+    for deadline in DEADLINES[2:]:
+        refine_positions(deadline, DEADLINES[1], cycles=10)
+
+
+if __name__ == "__main__":
+    main()
