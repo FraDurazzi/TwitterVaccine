cd42d67bc4a33fc34045c28dce5c5fd63878d342
Mario Orlandini <mario.orlandini2@str957-bio11.personale.dir.unibo.it>
2024-07-25 14:44:13 +0200

    kfold implementation

--- Classification.py
+++ Classification.py
@@ -24,6 +24,7 @@ Directory Constants:
 from sentence_transformers import SentenceTransformer
 from sklearn.linear_model import LogisticRegressionCV,RidgeClassifierCV
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold 
 import numpy as np
 import pandas as pd
 import re,os
@@ -32,6 +33,7 @@ from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_sc
 from scipy.stats import bootstrap
 from pathlib import Path
 from dirs import TRANSFORMERS_CACHE_DIR, DATA_DIR, LARGE_DATA_DIR
+fold=True
 os.environ['TRANSFORMERS_CACHE'] = TRANSFORMERS_CACHE_DIR
 penalty='l2'
 solver='lbfgs'
@@ -45,6 +47,10 @@ if model=="logistic":
     settings=penalty+"_"+solver+"_"+method
 else:
     settings=model
+if fold:
+    settings=settings+"/fold"
+else:
+    settings=settings+"/bootstrap"
 WORKING_PATH=DATA_DIR+settings+"/"+str(len(labels))+"l/"
 Path(WORKING_PATH).mkdir(parents=True, exist_ok=True)
 
@@ -92,6 +98,215 @@ def compute_metrics(predictions: np.ndarray, labels: np.ndarray,method="bca") ->
             'single_class_int_conf_f1_score': f1s_conf,
             'matthews':matt,
             'int_conf_matthews': boot_int_matt._asdict()}
+    
+def compute_metrics_k_fold(predictions: np.ndarray, labels: np.ndarray,method="bca") -> dict:
+    """
+    Computes various performance metric.
+
+    Parameters:
+        predictions (np.ndarray): Predicted labels.
+        labels (np.ndarray): True labels.
+
+    Returns:
+        dict: A dictionary containing accuracy, F1 scores, Matthews correlation coefficient.
+    """
+    acc = np.mean(predictions == labels)
+    f1 = f1_score(labels, predictions, average = 'micro')
+    f1s= f1_score(labels, predictions, average = None)
+    matt=matthews_corrcoef(labels, predictions)
+
+    return {'accuracy': acc,
+            'f1_score': f1,
+            'f1_scores': f1s.tolist(),
+            'matthews':matt}
+    
+def bootstrap_class(train_df: pd.DataFrame, 
+                    val_df: pd.DataFrame, 
+                    test_df: pd.DataFrame, 
+                    fut_df: pd.DataFrame, 
+                    model: str=model, 
+                    l1_ratios: float = 0, 
+                    solver: str = solver, 
+                    random_state: int = 42, 
+                    max_iter: int = 10000, 
+                    using_cols: list = using_cols, 
+                    using: str = using) -> dict:
+    """
+    Trains a classification model using logistic regression or ridge classifier, 
+    makes predictions on train, validation, test, and future datasets, 
+    and computes metrics for each dataset.
+
+    Parameters:
+    -----------
+    train_df : pd.DataFrame
+        Training dataset containing features and labels.
+    
+    val_df : pd.DataFrame
+        Validation dataset containing features and labels.
+    
+    test_df : pd.DataFrame
+        Test dataset containing features and labels.
+    
+    fut_df : pd.DataFrame
+        Future dataset containing features and labels.
+    
+    model : str
+        The type of model to use. Options are "logistic" for logistic regression or 
+        any other string for ridge classifier.
+    
+    l1_ratios : float, optional (default=0)
+        The Elastic-Net mixing parameter, with 0 <= l1_ratios <= 1. Only used if 
+        model is "logistic".
+    
+    solver : str, optional (default='lbfgs')
+        Algorithm to use in the optimization problem for logistic regression.
+    
+    random_state : int, optional (default=42)
+        Seed used by the random number generator.
+    
+    max_iter : int, optional (default=10000)
+        Maximum number of iterations taken for the solvers to converge.
+    
+    using_cols : list, optional (default=None)
+        List of columns to use for training the model.
+    
+    using : str, optional (default='classification')
+        Specifies the task type. Can be used to handle different tasks such as 
+        'classification' or others.
+
+    Returns:
+    --------
+    results : dict
+        Dictionary containing the evaluation metrics for train, validation, test, 
+        and future datasets.
+    """
+    if model=="logistic":
+            if (l1_ratios):
+                clf=LogisticRegressionCV(penalty=penalty,
+                                         solver=solver,
+                                         random_state=random_state,
+                                         max_iter=max_iter,
+                                         l1_ratios=[l1_ratios],
+                                         cv=5).fit(train_df[using_cols],train_df["label"])
+            else:
+                clf=LogisticRegressionCV(penalty=penalty,
+                                         solver=solver,
+                                         random_state=random_state,
+                                         max_iter=max_iter,
+                                         cv=5).fit(train_df[using_cols],train_df["label"])
+        else:
+            clf=RidgeClassifierCV().fit(train_df[using_cols],train_df["label"])
+        train_df["prediction"]=clf.predict(train_df[using_cols])
+        val_df["prediction"]=clf.predict(val_df[using_cols])
+        test_df["prediction"]=clf.predict(test_df[using_cols])
+        fut_df["prediction"]=clf.predict(fut_df[using_cols])
+        results={"Train set": compute_metrics(train_df["prediction"],train_df["label"],method),
+             "Val set":compute_metrics(val_df["prediction"],val_df["label"],method),
+             "Test set":compute_metrics(test_df["prediction"],test_df["label"],method),
+             "Fut set":compute_metrics(fut_df["prediction"],fut_df["label"],method)}
+
+def kfold_class(fold_df: pd.DataFrame, 
+                    test_df: pd.DataFrame, 
+                    fut_df: pd.DataFrame,
+                    model: str=model, 
+                    l1_ratios: float = 0, 
+                    solver: str = solver, 
+                    random_state: int = 42, 
+                    max_iter: int = 10000, 
+                    using_cols: list = using_cols, 
+                    using: str = using) -> dict:
+"""
+    Trains a classification model using logistic regression or ridge classifier with K-fold cross-validation,
+    makes predictions on test and future datasets, and computes metrics for each dataset.
+
+    Parameters:
+    -----------
+    fold_df : pd.DataFrame
+        Dataset to be used for K-fold cross-validation containing features and labels.
+    
+    test_df : pd.DataFrame
+        Test dataset containing features and labels.
+    
+    fut_df : pd.DataFrame
+        Future dataset containing features and labels.
+    
+    model : str
+        The type of model to use. Options are "logistic" for logistic regression or 
+        any other string for ridge classifier.
+    
+    l1_ratios : float, optional (default=0)
+        The Elastic-Net mixing parameter, with 0 <= l1_ratios <= 1. Only used if 
+        model is "logistic".
+    
+    solver : str, optional (default='lbfgs')
+        Algorithm to use in the optimization problem for logistic regression.
+    
+    random_state : int, optional (default=42)
+        Seed used by the random number generator.
+    
+    max_iter : int, optional (default=10000)
+        Maximum number of iterations taken for the solvers to converge.
+    
+    using_cols : list, optional (default=None)
+        List of columns to use for training the model.
+    
+    using : str, optional (default='classification')
+        Specifies the task type, such as 'classification'.
+
+    Returns:
+    --------
+    results : Dict[str, Dict[str, Any]]
+        Dictionary containing the evaluation metrics for train, validation, test, 
+        and future datasets.
+"""
+    kf = KFold(n_splits=5)
+    kf.get_n_splits(fold_df.index.tolist())
+    for i, (train_index, val_index) in enumerate(kf.split(X)):
+        if model=="logistic":
+            if (l1_ratios):                
+                clf=LogisticRegressionCV(penalty=penalty,
+                                         solver=solver,
+                                         random_state=random_state,
+                                         max_iter=max_iter,
+                                         l1_ratios=[l1_ratios],
+                                         cv=5).fit(fold_df[using_cols].loc[train_index],fold_df["label"].loc[train_index])
+            else:
+                clf=LogisticRegressionCV(penalty=penalty,
+                                         solver=solver,
+                                         random_state=random_state,
+                                         max_iter=max_iter,
+                                         cv=5).fit(fold_df[using_cols].loc[train_index],fold_df["label"].loc[train_index])
+        else:
+            clf=RidgeClassifierCV().fit(fold_df[using_cols].loc[train_index],fold_df["label"].loc[train_index])
+        train_pred=clf.predict(fold_df[using_cols].loc[train_index])
+        val_pred=clf.predict(fold_df[using_cols].loc[val_index])
+        partial_results_train={'accuracy':np.empty(shape=5),
+                       'f1_score':np.empty(shape=5),
+                       'f1_scores':np.empty(shape=(5,3)),
+                       'matthews':np.empty(shape=5)}
+        esteem=compute_metrics_k_fold(train_pred,fold_df["label"].loc[train_index].to_numpy())
+        for j in list(esteem.keys()):
+            partial_results_train[j][i]=esteem[j]
+        partial_results_val={'accuracy':np.empty(shape=5),
+                     'f1_score':np.empty(shape=5),
+                     'f1_scores':np.empty(shape=(5,3)),
+                     'matthews':np.empty(shape=5)}
+        esteem=compute_metrics_k_fold(val_pred,fold_df["label"].loc[val_index].to_numpy())
+        for j in list(esteem.keys()):
+            partial_results_val[j][i]=esteem[j]
+    result_train={}    
+    result_val={} 
+    for j in list(partial_results_train.keys()):
+        result_train[j]=np.mean(partial_results_train[j],axis=0)
+        result_train[j+"_error"]=np.std(partial_results_train[j],axis=0)
+        result_val[j]=np.mean(partial_results_val[j],axis=0)
+        result_val[j+"_error"]=np.std(partial_results_val[j],axis=0)        
+    test_df["prediction"]=clf.predict(test_df[using_cols])
+    fut_df["prediction"]=clf.predict(fut_df[using_cols])
+    results={"Train set": result_train,
+             "Val set":result_val,
+             "Test set":compute_metrics(test_df["prediction"],test_df["label"],method),
+             "Fut set":compute_metrics(fut_df["prediction"],fut_df["label"],method)}
 
 def loader(kind: str) -> pd.DataFrame:
     """
@@ -123,14 +338,16 @@ def main():
         val_df=loader("val_2l")
         test_df=loader("test_2l")
         fut_df=loader("fut_2l")
+        fold_df=loader("fold_2l")
     else:
         train_df=loader("train")
         val_df=loader("val")
         test_df=loader("test")
         fut_df=loader("fut")
-        
+        fold_df=loader("fold")   
     filename="output.txt"
     fileout="output.json"
+    f=open(WORKING_PATH+filename,"w")   
     ###Rescaling the used feature
     rescale=StandardScaler()
     rescale.fit(train_df[using_cols])
@@ -138,36 +355,13 @@ def main():
     val_df[using_cols]=rescale.transform(val_df[using_cols])
     test_df[using_cols]=rescale.transform(test_df[using_cols])
     fut_df[using_cols]=rescale.transform(fut_df[using_cols])
+    fold_df[using_cols]=rescale.transform(fold_df[using_cols])
     ###
-    if model=="logistic":
-        if (l1_ratios):
-            clf=LogisticRegressionCV(penalty=penalty,
-                                     solver=solver,
-                                     random_state=42,
-                                     max_iter=10000,
-                                     l1_ratios=[l1_ratios],
-                                     cv=5).fit(train_df[using_cols],train_df["label"])
-        else:
-            clf=LogisticRegressionCV(penalty=penalty,
-                                     solver=solver,
-                                     random_state=42
-                                     ,max_iter=10000,
-                                     cv=5).fit(train_df[using_cols],train_df["label"])
+    if (not fold):
+        results=bootstrap_class(train_df,val_df,test_df,fut_df)
     else:
-        clf=RidgeClassifierCV().fit(train_df[using_cols],train_df["label"])
-    try:
-        f=open(WORKING_PATH+filename,"x") 
-    except FileExistsError:
-        f=open(WORKING_PATH+filename,"a")   
+        results=kfold_class(fold_df,test_df,fut_df)
     f.write(using+":\n")
-    train_df["prediction"]=clf.predict(train_df[using_cols])
-    val_df["prediction"]=clf.predict(val_df[using_cols])
-    test_df["prediction"]=clf.predict(test_df[using_cols])
-    fut_df["prediction"]=clf.predict(fut_df[using_cols])
-    results={"Train set": compute_metrics(train_df["prediction"],train_df["label"],method),
-             "Val set":compute_metrics(val_df["prediction"],val_df["label"],method),
-             "Test set":compute_metrics(test_df["prediction"],test_df["label"],method),
-             "Fut set":compute_metrics(fut_df["prediction"],fut_df["label"],method)}
     for i in results.keys():
         f.write("\t "+ i+": \n")
         for j in results[i].keys():
@@ -197,7 +391,7 @@ if __name__ == "__main__":
     norm_lab_prop=['norm_lab_prop_0', 'norm_lab_prop_1', 'norm_lab_prop_2', 'norm_lab_prop_3']
     text_cols=["emb_col_"+str(i) for i in range(768)]
     features=[n2v,leiden,louvain,lap,fa2,lab_prop,norm_lap,norm_leiden,norm_louvain,norm_lab_prop]
-    features_name=["n2v","leiden","louvain","lap","fa2","lab_prop","norm_lap","norm_leiden","norm_louvain"]
+    features_name=["n2v","leiden","louvain","lap","fa2","lab_prop","norm_lap","norm_leiden","norm_louvain","norm_lab_prop"]
     using="norm_lap"
     using_cols=norm_lap
     results={}
@@ -212,10 +406,7 @@ if __name__ == "__main__":
         using=features_name[i]+" + text"
         using_cols=np.append(features[i],text_cols)        
         results[using]=main()
-        fileout="output.json"
-    try:
-        f=open(WORKING_PATH+fileout,"x") 
-    except FileExistsError:
-        f=open(WORKING_PATH+fileout,"a")
+    fileout="output.json"
+    f=open(WORKING_PATH+fileout,"w")
     json.dump(results,f)
     f.close()
